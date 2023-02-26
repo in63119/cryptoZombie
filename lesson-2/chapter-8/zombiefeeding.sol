7bee43fbcdf12a5c5d441d7b4312e8eabc7192fe
이인수 <in63119@gmail.com>
2023-02-26 16:18:10 +0900

    평균값 계산, 구조체의 포인터와 접근방법

--- lesson-2/chapter-8/zombiefeeding.sol
+++ lesson-2/chapter-8/zombiefeeding.sol
@@ -5,6 +5,8 @@ contract ZombieFeeding is ZombieFactory {
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
+
+    // 새로운 dna를 계산하는 방법
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     _createZombie("NoName", newDna);
