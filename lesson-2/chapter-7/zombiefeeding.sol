6a0cacf627d34b0e837cb7a5fb897a4cb35d7036
이인수 <in63119@gmail.com>
2023-02-26 16:11:18 +0900

    storage

--- lesson-2/chapter-7/zombiefeeding.sol
+++ lesson-2/chapter-7/zombiefeeding.sol
@@ -3,7 +3,9 @@ import "./zombiefactory.sol";
 contract ZombieFeeding is ZombieFactory {
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    // require로 좀비id만 가지고 있는 오너만 이 함수를 쓸 수 있다.
     require(msg.sender == zombieToOwner[_zombieId]);
+    // 기본적으로 함수에 있는 변수는 메모리에 저장 되지만, 이 storage가 붙인 변수는 블록체인에 저장된다.
     Zombie storage myZombie = zombies[_zombieId];
   }
 
