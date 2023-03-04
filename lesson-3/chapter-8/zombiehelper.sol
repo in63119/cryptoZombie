43b36ce54c341c8aa6f58d7e24f42cf11e17ade3
이인수 <in63119@gmail.com>
2023-03-04 15:52:26 +0900

    인수를 가질 수 있는 제어자

--- lesson-3/chapter-8/zombiehelper.sol
+++ lesson-3/chapter-8/zombiehelper.sol
@@ -2,7 +2,9 @@ pragma solidity ^0.4.25;
 import "./zombiefeeding.sol";
 contract ZombieHelper is ZombieFeeding {
 
+  // 제어자는 인수를 받을 수 있다.
   modifier aboveLevel(uint _level, uint _zombieId) {
+    // 현재는 _zombieId의 level이 인자로 들어오는 _level보다 이상인지 확인하고 있다.
     require(zombies[_zombieId].level >= _level);
     _;
   }
