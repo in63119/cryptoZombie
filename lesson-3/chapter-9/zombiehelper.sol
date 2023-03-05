4ee4cd2f161d95b707b6b6091161e91170b955fd
이인수 <in63119@gmail.com>
2023-03-05 13:36:09 +0900

    제어자를 사용

--- lesson-3/chapter-9/zombiehelper.sol
+++ lesson-3/chapter-9/zombiehelper.sol
@@ -7,11 +7,14 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
+  // aboveLevel을 사용해보는 시간.
+  // changeName에서는 레벨이 2보다 이상이어야 이름을 바꿀 수 있다.
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].name = _newName;
   }
 
+  // changeDna에서는 레벨이 20보다 이상이어야 DNA를 바꿀 수 있다.
   function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].dna = _newDna;
