4a7599e6711166bd7b797d933dcc6ea91ec13590
이인수 <in63119@gmail.com>
2023-03-06 22:09:10 +0900

    view 함수

--- lesson-3/chapter-10/zombiehelper.sol
+++ lesson-3/chapter-10/zombiehelper.sol
@@ -17,6 +17,10 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].dna = _newDna;
   }
 
+  // view 함수를 이용해 가스 절약하기
+  // view 함수는 외부에서 호출되었을 때 가스를 전혀 소모하지 않는다.
+  // 트랜잭션을 만들지 않기 때문이다.
+  // 외부에서 써야 하기 때문에 external과 view를 같이 써준다.
   function getZombiesByOwner(address _owner) external view returns(uint[]) {
 
   }
