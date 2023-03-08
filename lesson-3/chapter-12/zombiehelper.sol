f2a7537b8af160edd59ad38ba7a14ba7cadbb535
이인수 <in63119@gmail.com>
2023-03-08 22:19:59 +0900

    for문

--- lesson-3/chapter-12/zombiehelper.sol
+++ lesson-3/chapter-12/zombiehelper.sol
@@ -19,6 +19,11 @@ contract ZombieHelper is ZombieFeeding {
 
   function getZombiesByOwner(address _owner) external view returns(uint[]) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    
+    // 새로운 좀비를 만들 때마다 해당 소유자의 좀비 배열에 새 좀비 추가
+    // for문을 사용
+    // 앞으로 해당 좀비의 owner를 알아서 나중에 '소유권 변경'시
+    // 가스 소모를 줄이면서 owner를 알아낼 수 있다.
     uint counter = 0;
     for (uint i = 0; i < zombies.length; i++) {
       if (zombieToOwner[i] == _owner) {
