57acc99cdfefd5af8667544a16b548f71fdd381b
이인수 <in63119@gmail.com>
2023-03-12 13:59:18 +0900

    의사 난수

--- lesson-4/chapter-4/zombieattack.sol
+++ lesson-4/chapter-4/zombieattack.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.25;
 import "./zombiehelper.sol";
 contract ZombieAttack is ZombieHelper {
+  // 의사 난수를 생성하는 방법
+  // 여기서는 keccak을 사용하여 의사 난수를 생성하였다. 
   uint randNonce = 0;
 
   function randMod(uint _modulus) internal returns(uint) {
