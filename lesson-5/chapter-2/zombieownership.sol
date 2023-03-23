35b5260bc5d11b8d0684bffea5b7abc4166679f9
이인수 <in63119@gmail.com>
2023-03-23 21:41:08 +0900

    erc721 상속

--- lesson-5/chapter-2/zombieownership.sol
+++ lesson-5/chapter-2/zombieownership.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.4.25;
 
 import "./zombieattack.sol";
+
+// erc721을 상속하는 챕터
+// 의문점? erc721.sol을 임포트 헀는데, 상속은 ERC721로 함.
+// 이거 괜찮나?
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
