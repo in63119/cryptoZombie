59cdf8a7df54bf9f2c4708b85162d4470f2b2644
이인수 <in63119@gmail.com>
2023-03-28 20:58:29 +0900

    transfer

--- lesson-5/chapter-5/zombieownership.sol
+++ lesson-5/chapter-5/zombieownership.sol
@@ -13,6 +13,10 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     return zombieToOwner[_tokenId];
   }
 
+  // ERC 721의 transfer 함수를 만든다.
+  // _to의 좀비 수는 증가, _from의 좀비 수는 감소
+  // 해당 토큰의 오너를 _to로 변경
+  // Transfer 이벤트  emit
   function _transfer(address _from, address _to, uint256 _tokenId) private {
     ownerZombieCount[_to]++;
     ownerZombieCount[_from]--;
