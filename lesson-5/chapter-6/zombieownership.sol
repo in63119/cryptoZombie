ff24d774a38f632677664963ac9308bc208ec295
이인수 <in63119@gmail.com>
2023-03-29 21:32:21 +0900

    transfer2

--- lesson-5/chapter-6/zombieownership.sol
+++ lesson-5/chapter-6/zombieownership.sol
@@ -22,6 +22,9 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     emit Transfer(_from, _to, _tokenId);
   }
 
+  // 여기서는 transferFrom이지만, 웹에서는 transfer였음.
+  // onlyOwnerOf 를 추가 해야하는데, 여기서는 바뀌었음.
+  // payable 을 추가해야하는데 웹에서는 설명이 없었음.
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
     require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
     _transfer(_from, _to, _tokenId);
