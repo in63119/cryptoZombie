5209359defc191901e6ac0c46c27ab46c86c035d
이인수 <in63119@gmail.com>
2023-03-30 22:59:54 +0900

    approve

--- lesson-5/chapter-7/zombieownership.sol
+++ lesson-5/chapter-7/zombieownership.sol
@@ -5,6 +5,7 @@ import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+  // 맵핑을 만들어다 전송할 토큰에 대한 승인을 한다.
   mapping (uint => address) zombieApprovals;
 
   function balanceOf(address _owner) external view returns (uint256) {
@@ -27,8 +28,10 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     _transfer(_from, _to, _tokenId);
   }
 
+  // approve 함수로 해당 토큰에 대한 승인을 받는다.
   function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
     zombieApprovals[_tokenId] = _approved;
-  }
 
+    // ERC721에 있는 Approval 이벤트를 호출
+    emit Approval(msg.sender, _approved, _tokenId);
 }
